d755bc8fbdb79831761a7b51a33218e73bac7488
Jacob Short <jrshort89@gmail.com>
2020-09-03 23:46:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,6 +4,7 @@ require 'pry'
 def key_for_min_value(name_hash)
   min = 0
   ans = nil
+<<<<<<< HEAD
   name_hash.each {
     |key, value| 
     if min == 0 and name_hash != {}
@@ -11,6 +12,17 @@ def key_for_min_value(name_hash)
       ans = key
     end
     if value < min
+=======
+  ts = {}
+  name_hash.each {
+    |key, value| 
+    if name_hash != {}
+      min = value
+      ans = key
+    end
+    binding.pry
+    if true and value < min
+>>>>>>> 56722bfee5a8f73b97f1d4bba8ffa47a648e804c
       min = value
       ans = key
     end
